8b6b7ade5e1f1cfb765619996dda0260bc2fb717
Chris Nady <nady.christopher@gmail.com>
2017-08-25 16:30:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 function deleteFromObjectByKey(object, key) {
   var newObject = object;
   delete newObject[key];
-  return object, newObject;
+  return [newObject, object];
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
